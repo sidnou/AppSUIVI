6cedbcb46fe921df3737ef52f6f4c3591770d5c0
jasid <sidjack972@gmail.com>
2023-03-08 17:36:28 +0100

    Création Application

--- src/bd.py
+++ src/bd.py
@@ -1,10 +1,13 @@
 import sqlite3
 
-connex = sqlite3.connect('suivi-Chronopost.db')
-c = connex.cursor()
+def creation_db():
+    connex = sqlite3.connect('suivi-Chronopost.db')
+    c = connex.cursor()
 
+    c.execute(""" CREATE TABLE IF NOT EXISTS suivi_chronopost (id INTEGER PRIMARY KEY AUTOINCREMENT, numero_suivi INTEGER UNIQUE, numero_dossier VARCHAR)""")
 
-c.execute(""" CREATE TABLE IF NOT EXISTS suivi_chronopost (id INTEGER PRIMARY KEY AUTOINCREMENT, numero_suivi INTEGER UNIQUE, numero_dossier VARCHAR)""")
+    connex.commit()
+    connex.close
 
-connex.commit()
-connex.close
+class DataBase():
+    pass
