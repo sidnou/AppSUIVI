5a66e32c7a5e9e2a47d45c556392184d731777b1
j-asi <sidjack972@gmail.com>
2023-03-10 23:28:14 +0100

    Création Application
    Ajout Fichier test_db.py
    
    Ajout de la fonction add_data et check_data

--- src/bd.py
+++ src/bd.py
@@ -1,13 +1,37 @@
 import sqlite3
 
+
 def creation_db():
     connex = sqlite3.connect('suivi-Chronopost.db')
     c = connex.cursor()
 
-    c.execute(""" CREATE TABLE IF NOT EXISTS suivi_chronopost (id INTEGER PRIMARY KEY AUTOINCREMENT, numero_suivi INTEGER UNIQUE, numero_dossier VARCHAR)""")
+    c.execute(
+        """ CREATE TABLE IF NOT EXISTS suivi_chronopost (id INTEGER PRIMARY KEY AUTOINCREMENT,
+         numero_suivi INTEGER UNIQUE, 
+         numero_dossier VARCHAR """)
 
     connex.commit()
-    connex.close
+    connex.close()
 
-class DataBase():
-    pass
+
+class DataBase:
+    connex = sqlite3.connect('suivi-Chronopost.db')
+    c = connex.cursor()
+
+    def check_data(self, suivi):
+        connex.execute("""SELECT COUNT(*) FROM suivi_chronopost WHERE numero_suivi = ?""", suivi)
+        if connex.fetchone()[0] > 0:
+            print("Le numèro de suivi existe déjà")
+
+    # Ajout de donnée
+    def add_data(self, numero_suivi, numero_dossier):
+        connex.execute("INSERT INTO suivi_chronopost (numero_dossier,numero_suivi) VALUES (?,?)", numero_dossier,
+                       numero_suivi)
+
+    # suppression de donnée
+    def supr_data(self):
+        pass
+
+
+if __name__ == '__main__':
+    creation_db()
