e195d8d993e3412ace5b482b9e51bf804993916c
jasid <sidjack972@gmail.com>
2023-03-14 13:41:05 +0100

    Creation Application Création de l'interface graphique utilisateur

--- src/gui.py
+++ src/gui.py
@@ -1,18 +1,82 @@
 from tkinter import *
+#####################################################################
+# ## Exemple
+# # Code
+# fenetre = Tk()
+# fenetre.title("Interface Utilisateur")
+# fenetre.geometry("600x600") # Dimension de la fenêtre
+# # Label
+# label = Label(fenetre, text="Bonjour le Monde")
+# label.pack()
+# # Canvas
+# canvas = Canvas(fenetre,width=400,height=400,background='white')
+# canvas.pack(side=TOP)
+
+# # Bouton
+# bouton = Button(fenetre,text="Impression")
+# bouton.pack()
+
+# # Entrée
+# valeur = StringVar() # Déclaration de la valeur
+# valeur.set("Valeur")
+# entree = Entry(fenetre,textvariable=valeur,width=30)
+# entree.pack()
+
+# fenetre.mainloop()
+
+######################################################################
+######################################################################
+
+fenetre = Tk() # fenêtre 
+
+fenetre.title("Suivi Envoi Chronopost") # Titre de la fenêtre
+
+fenetre.geometry("600x850") # Dimension de la fenêtre
+
+cadre = Frame(fenetre,width=500,height=600,bg="white",relief="solid", bd=1)
+cadre.place(x=50,y=20)
+# cadre.pack()
+
+entree1 = Entry(cadre)
+entree1.pack()
+entree2 = Entry(cadre)
+entree1.pack()
+entree3= Entry(cadre)
+entree1.pack()
+entree4= Entry(cadre)
+entree1.pack()
+entree5= Entry(cadre)
+entree1.pack()
+entree6= Entry(cadre)
+entree1.pack()
+entree7= Entry(cadre)
+entree1.pack()
+entree8= Entry(cadre)
+entree1.pack()
+entree9= Entry(cadre)
+entree1.pack()
+entree10= Entry(cadre)
+entree1.pack()
+entree11= Entry(cadre)
+entree1.pack()
+entree12= Entry(cadre)
+entree1.pack()
+entree13= Entry(cadre)
+entree1.pack()
+entree14= Entry(cadre)
+entree1.pack()
+entree15= Entry(cadre)
+entree1.pack()
+entree16= Entry(cadre)
+entree1.pack()
+entree17= Entry(cadre)
+entree1.pack()
+entree18= Entry(cadre)
+entree1.pack()
+entree19= Entry(cadre)
 
 
-# Code
-fenetre = Tk()
-fenetre.geometry("800x800") # Dimension de la fenêtre
-# Label 
-label = Label(fenetre, text="Bonjour le Monde")
-label.pack()
-# Canvas 
-canvas = Canvas(fenetre,width=400,height=400,background='white')
-canvas.pack(side=TOP)
-# Bouton
-bouton = Button(fenetre,text="Impression")
-bouton.pack()
 
 
 fenetre.mainloop()
+
