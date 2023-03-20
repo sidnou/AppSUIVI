6e8e46c29dc94713047652f11c9f7d6b8122a521
jasid <sidjack972@gmail.com>
2023-03-20 13:37:00 +0100

    Création Application Création PDF

--- src/gen_pdf.py
+++ src/gen_pdf.py
@@ -9,13 +9,11 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 class GenPdf():
 
     def __init__(self, data: set, nom_fichier: str, titre):
-        # for i in range(40):
-        #     self.data.append(i)
         self.data = data
         print(data)
         self.nom_fichier = nom_fichier
         self.titre = titre
-        self.table = Table(self.data, colWidths=[60*mm, 60*mm, 60*mm])
+        self.table = Table(self.data, colWidths=[80*mm, 80*mm]) # dimension des cellule du tableau
         self.style = TableStyle([
             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
@@ -27,17 +25,18 @@ class GenPdf():
             ('GRID', (0, 0), (-1, -1), 1, colors.black)
         ])
         self.table.setStyle(self.style)
+        
     # Création du fichier PDF
-
     def generateur_pdf(self):
         self.pdf = SimpleDocTemplate(self.nom_fichier, pagesize=letter)
         self.pdf.multiBuild([self.table])
 
 
+
 if __name__ =='__main__':
     data = [
-        ['test','test1','test2'],
-        ['dede','dede1','dede2']
+        ['Numéro de Suivi Chronopost','Numéro de Suivi Chronopost'],
+        ['Test 1','Test2']
             ]
     testPdf = GenPdf(data,"test-exemple-pdf.pdf","test")
     testPdf.generateur_pdf()
