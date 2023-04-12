ef8d56b42f4dc641cd895d69d118890df76ab0a1
j-asi <sidjack972@gmail.com>
2023-04-12 23:22:35 +0200

    Création Application
    Nettoyage et mise en page

--- src/gen_pdf.py
+++ src/gen_pdf.py
@@ -4,10 +4,10 @@ from reportlab.lib.units import mm
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from datetime import date
-from reportlab.pdfgen import canvas
 
 AUJOURD_HUI = date.today().strftime("%d/%m/%Y")
 ENTETE_PAGE = "ENVOI CHRONOPOST"
+
 
 # Classe Création d'un fichier PDF
 class GenPdf:
@@ -20,32 +20,30 @@ class GenPdf:
         self.data1 = [[f"Nombre de Colis: {nc}",
                        f"Date: {AUJOURD_HUI}\nNom et Prénom:\nSignature:\n\n\n"
                        ]]
-        self.data2 = [['RECEPTION DES COLIS PAR CHRONOPOST ']]
+
         self.nom_fichier = nom_fichier
         self.titre = titre
-        # Tableau des Numéros de suivis 
-        self.table = Table(self.data, colWidths=[80 * mm, 80 * mm])  # dimension des cellule du tableau
+
+        self.pdf = SimpleDocTemplate(self.nom_fichier, pagesize=letter)
+
+        # Tableau des Numéros de suivis
+        self.table = Table(self.data, colWidths=[53 * mm, 53 * mm, 53 * mm])  # dimension des cellule du tableau
         self.style = TableStyle([
             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('FONTSIZE', (0, 0), (-1, 0), 14),
-            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('FONTSIZE', (0, 0), (-1, 0), 10),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 10),
             ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
             ('GRID', (0, 0), (-1, -1), 1, colors.black)
         ])
         self.table.setStyle(self.style)
         # Tableau signature et non prénom
-        self.table1 = Table(self.data1, colWidths=[80 * mm, 80 * mm], spaceBefore=(15 * mm), rowHeights=(50 * mm))
+        self.table1 = Table(self.data1, colWidths=[80 * mm, 80 * mm], spaceBefore=(10 * mm), rowHeights=(50 * mm))
         self.style1 = TableStyle([('GRID', (0, 0), (-1, -1), 1, colors.black), ('VALIGN', (0, 0), (-1, -1), 'TOP'),
                                   ('FONTSIZE', (0, 0), (-1, 0), 12)])
         self.table1.setStyle(self.style1)
-
-        # self.table2 = Table(self.data2, colWidths=[80 * mm, 80 * mm], rowHeights=(25 * mm))
-        # self.style2 = TableStyle([('ALIGN', (0,0 ), (0, 0), 'CENTER'),
-        #                           ('FONTSIZE', (0, 0), (0, 0), 20)])
-        # self.table2.setStyle(self.style2)
 
         # En-tëte
 
@@ -59,7 +57,7 @@ class GenPdf:
         self.header = Paragraph(ENTETE_PAGE, self.styleH)
         self.header_table = Table([[self.header]])
         self.header_table.setStyle(TableStyle([
-            ("BACKGROUND", (0, 0), (-1, -1), colors.lightgrey),
+            ("BACKGROUND", (0, 0), (-1, -1), colors.white),
             ("TEXTCOLOR", (0, 0), (-1, -1), colors.black),
             ("ALIGN", (0, 0), (-1, -1), "CENTER"),
             ("FONTNAME", (0, 0), (-1, -1), "Helvetica-Bold"),
@@ -69,15 +67,19 @@ class GenPdf:
 
     # Création du fichier PDF
     def generateur_pdf(self):
-        self.pdf = SimpleDocTemplate(self.nom_fichier, pagesize=letter)
-        self.pdf.build([self.header_table, self.table, self.table1])
+        try:
+            self.pdf.build([self.header_table, self.table, self.table1])
+        except PermissionError:
+            print("Fichier déjà ouvert")
+
         # self.pdf.multiBuild([self.table2, self.table, self.table1])
 
 
 if __name__ == '__main__':
     data = [
-        ['Numéro de Suivi Chronopost', 'Numéro de Suivi Chronopost'],
-        ['Test 1', 'Test2']
+        ['Numéro de Suivi Chronopost', 'Numéro de Suivi Chronopost', 'Numéro de Suivi Chronopost']
     ]
+    for n in range(20):
+        data.append([f'Test {n}', f'Test {n}', f'test {n}'])
     testPdf = GenPdf(data, "test-exemple-pdf.pdf", "test", '15')
     testPdf.generateur_pdf()
