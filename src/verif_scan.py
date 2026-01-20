cf89a40cd7c12612dd17a441c56b50bfc2cb9f37
jasid <sidjack972@gmail.com>
2026-01-20 23:07:10 +0100

    Creation verif_scan

---
+++ src/verif_scan.py
@@ -0,0 +1,18 @@
+import re
+
+'''
+Script de verification qrcode scanné 
+
+- Recuperation des données spécifique 
+    ex: Numéro dossier M67S-AAMM-NNNNN (15 caratère incluant "-")
+        Numéro Suivi XRXXXXXXXXXFR (13 Caractères)
+
+
+A partir de n'importe que données scanné QRcode ou code barre
+- la longeur des code bare du numéro suivi sont 13
+- la longeur des qrcode sont superieurs a 13 
+
+
+'''
+
+
